dc1ea245f64d23541d25573637dcda8d590ae9a3
vagrant <vagrant@myvm19>
2021-07-29 09:23:34 +0000

    multiplication of matrices

--- multiply_p.c
+++ multiply_p.c
@@ -6,28 +6,27 @@
  * Assignment 1-C
 */
 #include <stdio.h>
-
 int main() {
-	printf("Enter matrix size ");
-	int n; // input size of matrix
-	scanf("%d",&n);
-	int mat[n][n]; // matrix mat
-	int pat[n][n]; // matrix pat 
-	printf("Enter matrix1 ");
-	for(int i=0;i<n;i++)
-	{
+    printf("Enter matrix size ");
+    int n; // input size of matrix
+    scanf("%d",&n);
+    int mat[n][n]; // matrix mat
+    int pat[n][n]; // matrix pat 
+    printf("Enter matrix1 ");
+    for(int i=0;i<n;i++)
+    {
 	    for(int j=0;j<n;j++)
 	    scanf("%d",&mat[i][j]); // input matrix mat
-	}
-	printf("Enter matrix2 ");
-	for(int i=0;i<n;i++)
-	{
+    }
+    printf("Enter matrix2 ");
+    for(int i=0;i<n;i++)
+    {
 	    for(int j=0;j<n;j++)
 	    scanf("%d",&pat[i][j]); // input matrix pat
-	}
-	int res[n][n]; // resultant matrix
-	for(int i=0;i<n;i++)
-	{
+    }
+    int res[n][n]; // resultant matrix
+    for(int i=0;i<n;i++)
+    {
 	    for(int j=0;j<n;j++)
 	    {	 
 	        res[i][j]=0; // set resultant matrix to 0
@@ -38,14 +37,14 @@ int main() {
 	        }
 	        res[i][j]=ans; // set resultant matrix to ans
 	    }
-	}
-	printf("Output Result\n");
-	for(int i=0;i<n;i++)
-	{
+    }
+    printf("Output Result\n");
+    for(int i=0;i<n;i++)
+    {
 	    for(int j=0;j<n;j++)
 	    printf("%d ",res[i][j]); // output resultant matrix
 	    printf("\n");
-	}
-	return 0;
+    }
+    return 0;
 }
 
