a89105d03891fa9564c2b61384883d5b5d44273d
vagrant <vagrant@myvm19>
2021-07-29 08:13:08 +0000

    right most substring

---
+++ right_most_substring.c
@@ -0,0 +1,44 @@
+/*
+ *  Program to find right most occurrence of s1 in s2
+ *  Compilation : gcc right_most_substring.c
+ *  Execution : ./a.out
+ *  @vasu , 1910990395 , 23/07/2021
+ *  Assignment 2
+*/
+#include <stdio.h>
+// Program to find right most occurrence of s1 in s2
+int right_most_substring(char *s1, char *s2)
+{
+    int M=0;
+    int N=0;
+    
+    // store size of s1
+    while(*(s1 + M)!='\0')
+    M++;
+    // store size of s2
+    while(*(s2 + N)!='\0')
+    N++;
+    
+    // check from rightmost possible substring
+    for (int i = N - M; i >=0 ; i--) {
+        int j;
+        for (j = 0; j < M; j++)
+            if (s2[i + j] != s1[j]) // if not equal
+                break;
+           if (j == M) // if equal return index
+              return i;
+    }
+    return -1; // not present in entire string
+}
+int main() {
+	char s1[100];
+	char s2[100];
+	printf("Enter string ");
+	scanf("%s",s1);
+	printf("Enter substring ");
+	scanf("%s",s2);
+	printf("Output : ");
+	printf("%d",right_most_substring(s2,s1));
+	printf("\n");
+	return 0;
+}
